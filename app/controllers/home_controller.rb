1013b8cbe50893c17c851c28895ba9b5fc9873cb
Damon Davison <damon@curatur.com>
2015-11-03 14:52:25 -0500

    Add stuff from Views 1 and most of 2 lecture

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    # current_cohort = Cohort.last
+    # @users = User.cohort(current_cohort).active
+    # @users = User.all
+    @users = User.active
+    # DO NOT HARDCODE: @user = User.where(name: "Sam")
+    @hello = "HELLO!!!"
+  end
+
+  def list
+    @users = User.all
   end
 end
