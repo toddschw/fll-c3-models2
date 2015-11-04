0e33b2c430a277b735642ee381e368156290acf6
Damon Davison <damon@curatur.com>
2015-11-04 13:57:37 -0500

    Remove comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,7 +68,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:name, :email, :active)
   end
